0c7f373c44e138b5644d35f920afd1ca93753181
Abdullah Yahya <MetriclyFrench@users.noreply.github.com>
2017-12-08 14:39:02 -0800

    Figured out how to clear array. Refactored back into 2 functions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,19 +1,25 @@
 'use strict';
+var numbersStore = [];
+
 // Business Logic
-function replaceIf(number) {
-  var numbersStore = [];
+function countUpTo(number) {
   for (var i = 0; i <= number; i++) {
-    if ((i == '0') || ((i % 3 != 0) && (i.toString().match(/[0]/)))) {
-      numbersStore.push('Beep!');
-    } else if ((i == '1') || ((i % 3 != 0) && (i.toString().match(/[1]/)))) {
-      numbersStore.push('Boop!');
-    } else if ((i == 3) || (i % 3 == 0)) {
-      numbersStore.push("I'm sorry, Dave. I'm afraid I can't do that");
-    } else {
-      numbersStore.push(i);
-    }
+    if (i == replaceIf(i))
+      replaceIf(i);
   }
   return numbersStore.join(' ');
+}
+
+function replaceIf(number) {
+  if ((number == '0') || ((number % 3 != 0) && (number.toString().match(/[0]/)))) {
+    numbersStore.push('Beep!');
+  } else if ((number == '1') || ((number % 3 != 0) && (number.toString().match(/[1]/)))) {
+    numbersStore.push('Boop!');
+  } else if ((number == 3) || (number % 3 == 0)) {
+    numbersStore.push("I'm sorry, Dave. I'm afraid I can't do that");
+  } else {
+    numbersStore.push(number);
+  }
 }
 
 // User Interface Logic
@@ -21,9 +27,10 @@ function beepBoopForm() {
   var beepBoopForm = document.getElementById('beepBoopForm');
   beepBoopForm.addEventListener('submit', function(event) {
     event.preventDefault();
+    numbersStore = [];
     var userInputField = parseInt($('#userInputField').val());
     var beepBoopDisplay = $('#beepBoopDisplay');
-    beepBoopDisplay.text(replaceIf(userInputField));
+    beepBoopDisplay.text(countUpTo(userInputField));
   });
 }
 
