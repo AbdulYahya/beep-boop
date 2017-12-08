f28262c6d7d21aaa3a76c1558eb9b3fbc40d76b1
Abdullah Yahya <MetriclyFrench@users.noreply.github.com>
2017-12-08 14:51:14 -0800

    Added input field to take into account the users name

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,8 @@
 'use strict';
 var numbersStore = [];
-
-// Business Logic
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
 function countUpTo(number) {
   for (var i = 0; i <= number; i++) {
     if (i == replaceIf(i))
@@ -9,29 +10,25 @@ function countUpTo(number) {
   }
   return numbersStore.join(' ');
 }
-
 function replaceIf(number) {
   if ((number == '0') || ((number % 3 != 0) && (number.toString().match(/[0]/)))) {
     numbersStore.push('Beep!');
   } else if ((number == '1') || ((number % 3 != 0) && (number.toString().match(/[1]/)))) {
     numbersStore.push('Boop!');
   } else if ((number == 3) || (number % 3 == 0)) {
-    numbersStore.push("I'm sorry, Dave. I'm afraid I can't do that");
+    numbersStore.push("I'm sorry, " + capitalize($('#userNameInputField').val()) + ". I'm afraid I can't do that");
   } else {
     numbersStore.push(number);
   }
 }
-
-// User Interface Logic
 function beepBoopForm() {
   var beepBoopForm = document.getElementById('beepBoopForm');
   beepBoopForm.addEventListener('submit', function(event) {
     event.preventDefault();
     numbersStore = [];
-    $('#beepBoopDisplay').text(countUpTo(parseInt($('#userInputField').val())));
+    $('#beepBoopDisplay').text(countUpTo(parseInt($('#userNumberInputField').val())));
   });
 }
-
 $(function(){
   beepBoopForm();
 });
