12a270c0bac2cfc8e4754f8b3a352e2dd11bac41
Abdullah Yahya <abdullah@>
2018-02-05 09:48:37 -0800

    Incorporated node and gulp

---
+++ gulpfile.js
@@ -0,0 +1,48 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var utilities = require('gulp-util');
+var del = require('del');
+var jshint = require('gulp-jshint');
+
+var buildProduction = utilities.env.production; // append '--production' to gulp command
+
+gulp.task('concatJS', function() {
+  return gulp.src(['./js/*.js', './js/signup-interface.js'])
+    .pipe(concat('allConcact.js'))
+    .pipe(gulp.dest('./tmp'));
+});
+
+gulp.task('jsBrowserify', ['concatJS'], function() {
+  return browserify({ entries: ['./tmp/allConcact.js'] })
+    .bundle()
+    .pipe(source('app.js'))
+    .pipe(gulp.dest('./build/js'));
+});
+
+gulp.task("minify", ["jsBrowserify"], function() {
+  return gulp.src("./build/js/app.js")
+    .pipe(uglify())
+    .pipe(gulp.dest("./build/js"));
+});
+
+gulp.task("clean", function() {
+  return del(['build', 'tmp']);
+});
+
+gulp.task("build", ['clean'], function() {
+  if (buildProduction) {
+    gulp.start('minify');
+  } else {
+    gulp.start('jsBrowserify');
+  }
+});
+
+// linter task
+gulp.task('jshint', function() {
+  return gulp.src(['js/*.js'])
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
